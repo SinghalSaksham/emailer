55fb029e6e812fddddb87b611091f46c798483ad
unknown <singhalsaksham.mzn@gmail.com>
2021-06-25 23:02:20 +0530

    Adding starting project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//import Dashboard from './components/Dashboard/Dashboard';
+//import Compose from './components/compose/Compose';
+// import SignInSide from './components/login/Login';
+// import SignUp from "./components/register/Register.js";
+import Sidebar from "./components/sideabar/Sidebar.js";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SignInSide /> */}
+      {/* <SignUp /> */}
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
